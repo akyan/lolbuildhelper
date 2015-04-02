2464f535cbcf8b11aa5e50018ac4b8a89785a56f
Akyan <akyan@scary-biscuits.com>
2015-04-02 09:42:42 +0100

    Fixed riot service integration test

--- test/lib/controllers/riotservice.js
+++ test/lib/controllers/riotservice.js
@@ -9,6 +9,18 @@ var service = new RiotService(config.riotApiConfig);
 
 describe('riotservice', function () {
 
+  // Riot service pre-loading of cache takes a while to start, so give it up-to 10 seconds.
+  before(function(done) {
+    this.timeout(10000);
+    var repeater = setInterval(function () {
+      if (service.isReadyForRequests())
+      {
+        clearInterval(repeater);
+        done();
+      }
+    }, 1000);
+  })
+
   it('should return valid formated URL for region', function(){
     var url = service.buildEndpointUrl(config.riotApiConfig.riotServiceMethods['summonerByName'], {region: 'euw', name: 'akyantor'});
     assert.equal(url, 'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/akyantor?api_key='+config.riotApiConfig.devkey);
@@ -22,8 +34,8 @@ describe('riotservice', function () {
     assert.equal(service.isValidRegion('abc'), false);
   })
 
-  it('should return correct summoner details for Akyan when finding by name', function(done) {
-    service.findSummonerByName('EUW', 'akyantor', function(result) {
+  it('should return correct summoner details for akyantor when finding by name', function(done) {
+    service.findSummonerByName('euw', 'akyantor', function(result) {
       var summonerId = result.id;
       assert.equal(summonerId, '40397382', 'Summoner Id should be 40397382');
       done();
