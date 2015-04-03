c63941597dd30c0ab9bcd056c391fb2572b58705
Akyan <akyan@scary-biscuits.com>
2015-04-04 00:47:35 +0100

    Hacked in basic query via service and displayed results

--- frontend/public/javascript/app.js
+++ frontend/public/javascript/app.js
@@ -2,6 +2,25 @@
 
 var buildFinderApp = angular.module('buildFinderApp', []);
 
-buildFinderApp.controller('titleCtrl', function($scope) {
-    $scope.title = 'LOL Build Assist';
+buildFinderApp.serv
+
+buildFinderApp.controller('titleCtrl', function($scope, $http) {
+
+    var apiUrl = location.protocol + '//' + location.hostname + ':2424';
+
+    $scope.title = 'LoL Build Assist';
+    $scope.summonerSearch;
+    $scope.summoner = null;
+
+    $scope.search = function() {
+        $http.get(apiUrl + '/summoner/euw/' + $scope.summonerSearch + '/champion')
+            .success(function (data, status, headers, config) {
+                $scope.summoner = data;
+                console.log(data);
+            })
+            .error(function(data, status, headers, config) {
+                $scope.summoner = null;
+                console.log(JSON.stringify(headers));
+            });
+    }
 });
