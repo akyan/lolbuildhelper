b74fd8981e8c18fba7af54e735af59adaab8d941
Akyan <akyan@scary-biscuits.com>
2015-04-03 20:03:40 +0100

    Added basic angular support for UI
    Added angular to project and exposed libraries via frontend UI service.

--- frontend/index.js
+++ frontend/index.js
@@ -6,6 +6,24 @@ var logger = require('../helpers/logger')('Front End');
 logger.log('Starting Up');
 
 webApp.use(express.static(path.join(__dirname, './public')));
+
+// Adding any npm loaded modules to be available for use via public facing site where appropriate.
+webApp.use('/javascript/angular.min.js', function (req, res, next) {
+    res.sendFile(path.join(__dirname,'../node_modules/angular/angular.min.js'));
+})
+
+webApp.use('/javascript/angular.min.js.map', function (req, res, next) {
+    res.sendFile(path.join(__dirname,'../node_modules/angular/angular.min.js.map'));
+})
+
+webApp.use('/javascript/angular-resource.min.js', function (req, res, next) {
+    res.sendFile(path.join(__dirname,'../node_modules/angular-resource/angular-resource.min.js'));
+})
+
+webApp.use('/javascript/angular-resource.min.js.map', function (req, res, next) {
+    res.sendFile(path.join(__dirname,'../node_modules/angular-resource/angular-resource.min.js.map'));
+})
+
 webApp.listen(8081);
 
 logger.log('Started');
---
+++ frontend/public/javascript/app.js
@@ -0,0 +1,7 @@
+
+
+var buildFinderApp = angular.module('buildFinderApp', []);
+
+buildFinderApp.controller('titleCtrl', function($scope) {
+    $scope.title = 'LOL Build Assist';
+});
